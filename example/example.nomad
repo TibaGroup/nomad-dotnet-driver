ea67de3a8f0173a082be1dc7f9a1e40548ccfc34
mamsi <m.hossein95th@gmail.com>
2024-09-16 08:55:17 +0330

    test: add test-resources project

--- example/example.nomad
+++ example/example.nomad
@@ -5,17 +5,27 @@ job "example" {
 
       config {
         dll_path = "${NOMAD_TASK_DIR}/TestNomadTask.dll"
+        app_name = "TestNomadTask"
         runtime_version = "8.0.8"
         threading {
           min_threads = 10
           max_threads = 100
         }
+        gc {
+          enable = true
+           }
         args = ["9090"]
       }
-
-      artifact {
-     source = "http://localhost/test_nomad_task.zip"
-     destination = "local"
+    resources {
+      network {
+        port "http" {
+          static = 9090
+        }
+      }
+    }
+     artifact {
+       source = "http://localhost/test_nomad_task.zip"
+       destination = "local"
       }
     }
     task "dotnet_test_2" {
@@ -23,17 +33,27 @@ job "example" {
 
           config {
             dll_path = "${NOMAD_TASK_DIR}/TestNomadTask.dll"
+            app_name = "TestNomadTask"
             runtime_version = "7.0.20"
             threading {
               min_threads = 10
               max_threads = 100
             }
+            gc {
+              enable = false
+            }
             args = ["8080"]
           }
-
+        resources {
+          network {
+            port "http" {
+              static = 8080
+            }
+          }
+        }
           artifact {
-         source = "http://localhost/test_nomad_task.zip"
-         destination = "local"
+            source = "http://localhost/test_nomad_task.zip"
+            destination = "local"
           }
         }
   }
---
+++ test-resources/TestNomadTask/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Self;
+using System.IO;
+using System.Runtime;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace TestNomadTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("NOMAD_HOST_PORT_http");
+            var uri = new Uri("http://localhost:" + port);
+            var hostConfig = new HostConfiguration { UrlReservations = new UrlReservations { CreateAutomatically = true } };
+            using (var host = new NancyHost(hostConfig, uri))
+            {
+                host.Start();
+                Console.WriteLine("NancyFX service running on " + uri);
+
+                // Keep the application running indefinitely
+                Task.Run(() => KeepRunning()).Wait();
+            }
+        }
+
+        private static void KeepRunning()
+        {
+            while (true)
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+    public class DictionaryConverter
+    {
+        public static Dictionary<string, string> ConvertToStringDictionary(Dictionary<string, object> original)
+        {
+            var stringDictionary = new Dictionary<string, string>();
+
+            foreach (var kvp in original)
+            {
+                stringDictionary[kvp.Key] = kvp.Value?.ToString();
+            }
+
+            return stringDictionary;
+        }
+    }
+   public class ConfigProperties
+   {
+       public bool System_GC_Server { get; set; }
+       public int System_Threading_ThreadPool_MaxThreads { get; set; }
+       public int System_Threading_ThreadPool_MinThreads { get; set; }
+   }
+
+   public class Framework
+   {
+       public string Name { get; set; }
+       public string Version { get; set; }
+   }
+
+   public class RuntimeOptions
+   {
+       public ConfigProperties ConfigProperties { get; set; }
+       public Framework Framework { get; set; }
+       public string Tfm { get; set; }
+   }
+
+   public class ConfigFile
+   {
+       public RuntimeOptions RuntimeOptions { get; set; }
+   }
+    public class MyModule : NancyModule
+    {
+        public MyModule()
+        {
+            Get("/__health", _ =>
+            {
+                var response = new
+                {
+                    status = "ok",
+                    datetime = DateTime.UtcNow
+                };
+                return Response.AsJson(response);
+            });
+
+            Get("/configs", _ =>
+            {
+                string text = File.ReadAllText("./local/TestNomadTask.runtimeconfig.json");
+                return Response.AsText(text);
+            });
+        }
+    }
+}
